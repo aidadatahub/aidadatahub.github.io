02138a2e56bfa6cf45f87305799f23cc844a777f
Shreyas Shivakumara <shrsh52@liu.se>
2022-11-17 11:33:20 +0100

    Comments removed

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,22 +41,3 @@ else
 fi
 
 bootstrap
-
-# function bootstrap(){
-#     local Green="\033[0;32m"
-    #   echo -e "${Green}Installation starts...."
-    #   export DEBIAN_FRONTEND=noninteractive
-    #   sudo apt install rbenv -y > /dev/null 2>&1
-#     echo -e "${Green}Installed rbenv"
-#     rbenv init
-#     echo -e "${Green}rbenv init"
-#     git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build > /dev/null 2>&1
-#     echo -e "${Green}installed ruby-build"
-#     rbenv install 2.7.4 > /dev/null 2>&1
-#     echo "installed rbenv version 2.7.4"
-#     rbenv local 2.7.4
-#     eval "$(rbenv init -)"
-#     bundle install > /dev/null 2>&1
-#     echo -e "${Green}installed bundle"
-#     bundle exec jekyll serve
-# }
