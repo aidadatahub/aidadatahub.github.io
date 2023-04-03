b577e0db3db32601043bc42d5e036d6c77de4b98
Shreyas Shivakumara <shrsh52@liu.se>
2023-04-03 22:46:48 +0200

    Replaced echo with printf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 ruby_installation() {
-    echo -e "${Green}Installation of ruby begins...."
+    printf "%s\n %s""${GREEN}Installation of ruby begins....${NORMAL}"
     export DEBIAN_FRONTEND=noninteractive
     sudo apt install rbenv -y > /dev/null 2>&1
-    echo -e "${Green}Installed rbenv"
+    printf "%s\n %s""${GREEN}Installed rbenv ${NORMAL}"
     rbenv init
-    echo -e "${Green}rbenv init"
+    printf "%s\n%s""${GREEN}rbenv init${NORMAL}"
 }
 
 ruby_build() {
     git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build > /dev/null 2>&1
-    echo -e "${Green}installed ruby-build"
+    printf "%s\n%s""${GREEN}installed ruby-build${NORMAL}"
     rbenv install 2.7.4 > /dev/null 2>&1
     echo "installed rbenv version 2.7.4"
     rbenv local 2.7.4
@@ -20,20 +20,23 @@ ruby_build() {
 bootstrap() {
     eval "$(rbenv init -)"
     bundle install > /dev/null 2>&1
-    echo -e "${Green}installed bundle"
+    printf "%s%s""${GREEN}installed bundle${NORMAL}\n"
     bundle exec jekyll serve -l -o
 }
 
-if ! command -v rbenv &> /dev/null;then
+if ! command -v rbenv > /dev/null 2>&1;then
     echo "rbenv is not installed"
     ruby_installation
 else
     echo "rbenv is already installed"
 fi
 
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
 rb="$(rbenv versions | grep '2.7.4')"
 
-if [[ $rb == *"2.7.4"* ]];then
+if [[ $rb = *"2.7.4"* ]];then
     echo "Ruby 2.7.4 is installed"
 else
     echo "Ruby 2.7.4 is not installed"
